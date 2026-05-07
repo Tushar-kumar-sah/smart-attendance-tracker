47d3335db05ee7a159deff333eb8ffa945e0f409
Tushar Kumar Sah <sah05tushar@gmail.com>
2026-05-07 16:23:03 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -8,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Attendance API Running...");
+  res.send("API Running...");
 });
 
 const PORT = process.env.PORT || 5000;
