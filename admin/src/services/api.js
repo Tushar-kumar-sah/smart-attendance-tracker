0e86d72e6621a471331782446dd5ba06e8e5e5fc
Tushar Kumar Sah <sah05tushar@gmail.com>
2026-05-10 01:26:52 +0530

    Fixed admin deployment

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -6,6 +6,9 @@ const API_BASE_URL =
 
 const api = axios.create({
   baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 api.interceptors.request.use((config) => {
